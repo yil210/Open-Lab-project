6cb5f604203b901e83aabaeef9adf79d659314a6
yil210 <yixiao.li@foxmail.com>
2019-01-22 23:01:51 -0500

    upload isd final

---
+++ open-lab-website/connection/Configs.php
@@ -0,0 +1,21 @@
+<?php
+    class Configs {
+        const AIR_TABLE_KEY = '**';
+        const AIR_TABLE_APP_ID = '**';
+        const CHANNEL_ID = "**";
+        const YOUTUBE_API_KEY = "**";
+
+        const SERVER_MAIL_ACCOUNT = "**";
+        const SERVER_MAIL_PASSWORD = "**";
+        const SERVER_SMTP_ADDRESS = "smtp.gmail.com";
+        const SERVER_SMTP_PORT = "465";
+
+        const SERVER_MAIL_FROM = "**";
+        const SERVER_MAIL_FROM_NAME = "Open-lab";
+        const SERVER_MAIL_REPLYTO = "**";
+        const SERVER_MAIL_REPLYTO_NAME = "Open-lab";
+        const SERVER_MAIL_SEND = "**";
+        const SERVER_MAIL_SEND_NAME = "Open-lab";
+
+    }
+ ?>
